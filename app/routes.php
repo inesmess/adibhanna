ab70643a6c9173a697370233d6b79f9a507e2a4d
Adib Hanna <adibhanna@gmail.com>
2015-02-13 14:31:05 +0200

    updates

--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,4 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
-Route::get('/experiences', function()
-{
-	return Redirect::to('http://lb.linkedin.com/in/adibhanna');
-});
+Route::get('/', 'HomeController@index');
